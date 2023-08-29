4be6f8c0ee6e377958fd7de7fbb5108cd34a5dcd
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2023-08-29 19:20:14 +0530

    Decorate-Class📔📔📔

--- person.rb
+++ person.rb
@@ -1,8 +1,11 @@
-class Person
+require_relative 'decorators/nameable'
+
+class Person < Nameable
   attr_reader :id
   attr_accessor :name, :age
 
   def initialize(id, name: 'Unknown', age: nil, parent_permission: true)
+    super()
     @id = id
     self.name = name
     self.age = age
@@ -19,5 +22,9 @@ class Person
 
   def can_use_services?
     of_age? || @parent_permission
+  end
+
+  def correct_name
+    @name
   end
 end
