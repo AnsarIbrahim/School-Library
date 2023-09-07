f4e242413fc9be83295b9c12c63c061291ad633a
Aamir <aamirasaram2107@gmail.com>
2023-09-07 12:09:16 +0200

    Update student tests: add test for assign to class and default values

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -2,8 +2,9 @@ require_relative '../school_library/library/student'
 
 describe Student do
   before :each do
-    @student = Student.new(25, name: 'John')
-    @student_with_permission = Student.new(16, name: 'Alice', parent_permission: true)
+    @student = Student.new(25)
+    @student_without_permission = Student.new(16, name: 'Alice', parent_permission: false)
+    @student_with_name = Student.new(25, name: 'John')
   end
 
   describe '#initialize' do
@@ -11,10 +12,27 @@ describe Student do
       expect(@student).to be_an_instance_of Student
     end
     it 'should assign the correct name' do
-      expect(@student.name).to eql 'John'
+      expect(@student.name).to eql 'Unknown'
     end
     it 'should assign the correct age' do
       expect(@student.age).to eql 25
+    end
+    it 'should assign the correct parent_permission' do
+      expect(@student.parent_permission).to eql true
+    end
+    it 'should override the default parent_permission' do
+      expect(@student_without_permission.parent_permission).to eql false
+    end
+    it 'should override the default name' do
+      expect(@student_with_name.name).to eql 'John'
+    end
+  end
+
+  describe '#assign_classroom' do
+    it 'assigns the correct classroom' do
+      my_classroom = Classroom.new('7E Upper')
+      @student.assign_to_classroom(my_classroom)
+      expect(@student.classroom.label).to eql '7E Upper'
     end
   end
 
