8f2d98ee3e1299926512a546ea5134904768a2cd
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2023-09-07 14:12:10 +0530

    clear_format

--- spec/spec_helper.rb
+++
@@ -1,4 +0,0 @@
-require 'minitest/autorun'
-require_relative '../school_library/decorators/capitalize_decorator'
-require_relative '../school_library/decorators/trimmer_decorator'
-require_relative '../school_library/library/person'
